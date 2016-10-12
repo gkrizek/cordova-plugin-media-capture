def79e0ef831a9ece1d3a5586445eda338afda60
Graham Krizek <graham@grahamkrizek.com>
2016-10-12 05:40:32 -0600

    trying something new

--- src/ios/CDVCapture.m
+++ src/ios/CDVCapture.m
@@ -265,7 +265,7 @@
         if ([pickerController respondsToSelector:@selector(cameraCaptureMode)]) {
             pickerController.cameraCaptureMode = UIImagePickerControllerCameraCaptureModeVideo;
             if (quality && [quality intValue] == 1) {
-                 pickerController.videoQuality = UIImagePickerControllerQualityTypeMedium;
+                 pickerController.videoQuality = UIImagePickerControllerQualityTypeIFrame960x540;
             }
             // pickerController.cameraDevice = UIImagePickerControllerCameraDeviceRear;
             // pickerController.cameraFlashMode = UIImagePickerControllerCameraFlashModeAuto;

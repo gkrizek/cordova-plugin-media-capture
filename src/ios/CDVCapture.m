9b8fbb7e40a5764513d9e287dd7ae9519fccf661
Graham Krizek <graham@grahamkrizek.com>
2016-10-12 07:45:00 -0600

    switched to High quality

--- src/ios/CDVCapture.m
+++ src/ios/CDVCapture.m
@@ -265,7 +265,7 @@
         if ([pickerController respondsToSelector:@selector(cameraCaptureMode)]) {
             pickerController.cameraCaptureMode = UIImagePickerControllerCameraCaptureModeVideo;
             if (quality && [quality intValue] == 1) {
-                 pickerController.videoQuality = UIImagePickerControllerQualityTypeIFrame1280x720;
+                 pickerController.videoQuality = UIImagePickerControllerQualityTypeHigh;
             }
             // pickerController.cameraDevice = UIImagePickerControllerCameraDeviceRear;
             // pickerController.cameraFlashMode = UIImagePickerControllerCameraFlashModeAuto;
